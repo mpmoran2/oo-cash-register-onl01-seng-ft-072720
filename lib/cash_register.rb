494a40dd8928df0d7317cf849d7c6b46bcf42128
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:44:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,20 +7,13 @@ class CashRegister
     @itmes = []
   end 
   
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
-  end
+  def add_item(title, price, quantity=1)
+    @total += price * quantity 
+    quantity.times do 
+      @items << title 
+    end 
+    @last_item = price * quantity 
+  end 
   
   def apply_discount
   end 
