404f6a3e1434d8c34eb0d7e8ef95e2c4086476df
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:45:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0 
     @discount = discount
-    @itmes = []
+    @items = []
   end 
   
   def add_item(title, price, quantity=1)
@@ -16,7 +16,13 @@ class CashRegister
   end 
   
   def apply_discount
-  end 
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
   
   def items
   end 
