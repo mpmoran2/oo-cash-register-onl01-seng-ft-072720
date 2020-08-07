17ad16763f7da73d7a9a3a389d92c01bd21b5f1a
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-07 17:39:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,6 +2,11 @@ class CashRegister
   attr_accessor :discount, :items, :last_transaction_amount, :total 
   
   def initialize()
+    @total = 0 
+    @discount = discount
+    @itmes = []
+  end 
   
+  def 
   
 end
